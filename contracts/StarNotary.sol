f5517a6394a04c27127002e6907442ad14d5d23c
Hossam Magdy <hossam.magdy@xing.com>
2021-12-19 02:46:59 +0100

    Update StarNotary.sol (Task 1)

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -56,7 +56,7 @@ contract StarNotary is ERC721 {
         view
         returns (string memory)
     {
-        //1. You should return the Star saved in tokenIdToStarInfo mapping
+        return tokenIdToStarInfo[_tokenId].name;
     }
 
     // Implement Task 1 Exchange Stars function
@@ -65,11 +65,25 @@ contract StarNotary is ERC721 {
         //2. You don't have to check for the price of the token (star)
         //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId1)
         //4. Use _transferFrom function to exchange the tokens.
+
+        address owner1 = ownerOf(_tokenId1);
+        address owner2 = ownerOf(_tokenId2);
+
+        require(msg.sender == owner1 || msg.sender == owner2);
+
+        _transfer(owner1, owner2, _tokenId1);
+        _transfer(owner2, owner1, _tokenId2);
     }
 
     // Implement Task 1 Transfer Stars
     function transferStar(address _to1, uint256 _tokenId) public {
         //1. Check if the sender is the ownerOf(_tokenId)
         //2. Use the transferFrom(from, to, tokenId); function to transfer the Star
+
+        address owner = ownerOf(_tokenId);
+
+        require(msg.sender == owner);
+
+        _transfer(owner, _to1, _tokenId);
     }
 }
